258a694dc0cc3f810f12bb428cc9d0740e8321f4
lhebia <lhebia@gmail.com>
2020-05-09 02:15:16 -0400

    Added get radio button functionality

--- scripts.js
+++ scripts.js
@@ -1,24 +1,43 @@
 // when document is ready
 $(document).ready(function(){
 
+    // Get value of the radio buttons
+
+    const getRadioBtnValue = () => {
+        const useUpperBtn = document.querySelector('#useUpper');
+        const useNumberBtn = document.querySelector('#useNumbers');
+        const useSpecialCharBtn = document.querySelector('#useSpecialChar');
+        const checkedButtons = [];
+        for (let button of [useUpperBtn, useNumberBtn, useSpecialCharBtn]) {
+            if (button.checked) {
+                checkedButtons.push(button.value);
+            }
+        }
+        return checkedButtons;
+    }
+
     // Create string of all characters for password & pass into array
 
-    const createPasswordArray = () => {
+    const createPasswordArray = (arr) => {
         const lowerAlphaLibrary = 'abcdefghijklmnopqrstuvwxyz';
         const upperAlphaLibrary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         const numberLibrary = '1234567890';
         const specialLibrary = '~!@#$%^&*()_+=-';
-        const fullstringLibrary = lowerAlphaLibrary + upperAlphaLibrary + numberLibrary + specialLibrary;
+        const selectedLibrary = lowerAlphaLibrary;
+        const fullstringLibrary = [];
+        for (let item in arr) {
+            fullstringLibrary = selectedLibrary.concat(item);
+        }
         const arrayLibrary = fullstringLibrary.split('');
         return arrayLibrary;
     }
 
-    const lowerAlphaLibrary = 'abcdefghijklmnopqrstuvwxyz';
-    const upperAlphaLibrary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numberLibrary = '1234567890';
-    const specialLibrary = '~!@#$%^&*()_+=-';
-    const fullstringLibrary = lowerAlphaLibrary + upperAlphaLibrary + numberLibrary + specialLibrary;
-    const arrayLibrary = fullstringLibrary.split('');
+    // const lowerAlphaLibrary = 'abcdefghijklmnopqrstuvwxyz';
+    // const upperAlphaLibrary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // const numberLibrary = '1234567890';
+    // const specialLibrary = '~!@#$%^&*()_+=-';
+    // const fullstringLibrary = lowerAlphaLibrary + upperAlphaLibrary + numberLibrary + specialLibrary;
+    // const arrayLibrary = fullstringLibrary.split('');
 
     // Create a random integer function
     const randInt = (number = 10) => {
@@ -51,4 +70,6 @@ $(document).ready(function(){
         }
         
     })
+
+    getRadioBtnValue();
 });
